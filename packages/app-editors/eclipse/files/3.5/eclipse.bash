01b4d664e1d0ba5b61e480985de7c9f443407c52
Cecil Curry <leycec@gmail.com>
2010-04-30 01:39:54 +1200

    app-editors/eclipse: 99.999...% working

--- packages/app-editors/eclipse/files/3.5/eclipse.bash
+++ packages/app-editors/eclipse/files/3.5/eclipse.bash
@@ -6,7 +6,7 @@
 # --------------------( SYNOPSIS                           )--------------------
 # This script launches Eclipse! The Eclipse exheres fabricated this script for
 # your convenience. Therefore, avoid modifying the contents of this script
-# directly; rather, queue patches against the Eclipse exheres. :)
+# directly; rather, queue patches against the original Eclipse exheres.
 #
 # --------------------( CONFIGURATION                      )--------------------
 # This script pulls its configuration from:
@@ -16,38 +16,30 @@
 #
 # If Eclipse performs poorly or otherwise complains, consider modifying the
 # minimum and maximum memory the configuration file allocates.
+#
+# --------------------( SEE ALSO                           )--------------------
+# http://wiki.eclipse.org/FAQ_How_do_I_run_Eclipse%3F#eclipse.ini
 
 # ....................{ ENVIRONMENT VARIABLES              }....................
-if [[ -f   "__ECLIPSE_CONFIG_FILE__" ]]; then
-    source "__ECLIPSE_CONFIG_FILE__"
-else
-    echo "\"__ECLIPSE_CONFIG_FILE__\" not found; assuming default configuration." 1>&2 
-fi
-
 if [[ -z "${ECLIPSE_HOME}" ]]; then
-	echo "\$ECLIPSE_HOME not set; please see \"__ECLIPSE_ENVIRO_FILE__\"." 1>&2
+	echo "\$ECLIPSE_HOME not set! Did you delete \"__ECLIPSE_ENVIRO_FILE__\"?" 1>&2
 	exit 1
 fi
 
-local ECLIPSE_BIN="${ECLIPSE_HOME}/eclipse"
+if [[ -f   "__ECLIPSE_CONFIG_FILE__" ]]; then
+    source "__ECLIPSE_CONFIG_FILE__"
+else
+    echo "\"__ECLIPSE_CONFIG_FILE__\" not found! Assuming default configuration." 1>&2 
+fi
+
+local ECLIPSE_BIN="${ECLIPSE_HOME}/bin/eclipse"
 
 if [[ ! -x "${ECLIPSE_BIN}" ]]; then
 	echo "\"${ECLIPSE_BIN}\" not found or not executable by this user." 1>&2
 	exit 1
 fi
 
-# Naturally, "gcj" needs a classpath massage.
-if [[ "$(eclectic java-jdk show)" == gcj* ]]; then 
-    export JAVA_PKG_CLASSMAP="${ECLIPSE_HOME}/eclipse.gcjdb"
-fi
-
-local VMARGS=( )
-[[ -n "${VMARGS_XMS}"            ]] && VMARGS+=( "-Xms${VMARGS_XMS}" )
-[[ -n "${VMARGS_XMX}"            ]] && VMARGS+=( "-Xmx${VMARGS_XMX}" )
-[[ -n "${VMARGS_XX_PERMSIZE}"    ]] && VMARGS+=( "-XX:PermSize=${VMARGS_XX_PERMSIZE}" )
-[[ -n "${VMARGS_XX_MAXPERMSIZE}" ]] && VMARGS+=( "-XX:MaxPermSize=${VMARGS_XX_MAXPERMSIZE}" )
-
 # ....................{ ECLIPSE!                           }....................
-echo "${ECLIPSE_BIN}" "$@" -vmargs "${VMARGS[@]}"
-exec "${ECLIPSE_BIN}" "$@" -vmargs "${VMARGS[@]}"
+echo "${ECLIPSE_BIN}" "$@"
+exec "${ECLIPSE_BIN}" "$@"
 
